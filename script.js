46e2fa79060f3d04ef7bf9a7132a8ea8f8f9630d
Asif520 <57280266+Asif520@users.noreply.github.com>
2022-04-09 02:25:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+window.onscroll = () =>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+});
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
+
+
+// contact form validation
+function validateForm(){
+  let a = document.forms["contact-form"]["name"].value;
+  let b = document.forms["contact-form"]["email"].value;
+  let c = document.forms["contact-form"]["number"].value;
+  let d = document.forms["contact-form"]["subject"].value;
+  let e = document.forms["contact-form"]["message"].value;
+  if (a== ""){
+      alert("Name is missing");
+      return false;
+  }
+  if (b== ""){
+    alert("Email is missing");
+    return false;
+  }
+  if (c== ""){
+    alert("Please provide contact number");
+    return false;
+  }
+  if (d== ""){
+    alert("Fill out subject box");
+    return false;
+  }
+  if (e== ""){
+      alert("Please Drop A Message");
+      return false;
+  }
+  alert( "Message Send Successfully.");
+  }
